1b991f5273310237b89c3a58e50ac1cbeab14dea
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-06-11 14:48:06 -0400

    Make minor adjustments to navigation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 document.getElementById("nav-btn").addEventListener("click", () => {
-  document.getElementById("navigation").classList.toggle("open");
+  const navigation = document.getElementById("navigation");
+  const navBtnImg = document.getElementById("nav-btn-img");
+  navigation.classList.toggle("open");
   document.body.classList.toggle("scroll-lock");
+  if (navigation.classList.contains("open")) {
+    navBtnImg.src = "images/icon-close.svg";
+  } else {
+    navBtnImg.src = "images/icon-hamburger.svg";
+  }
 });
