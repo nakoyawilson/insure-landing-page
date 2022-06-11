691b3813e86ce97a97676aabd89c3a6b31221a11
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-06-11 14:19:02 -0400

    Change navigation to position fixed and add scroll lock

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 document.getElementById("nav-btn").addEventListener("click", () => {
   document.getElementById("navigation").classList.toggle("open");
+  document.body.classList.toggle("scroll-lock");
 });
